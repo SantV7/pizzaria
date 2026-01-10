a9a8bba12273704d18c7b2798bc905842ec68f83
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-01-10 03:48:45 -0300

    area info on promotion

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react'
 import '../styles/main.css'
 import ScrollReveal from 'scrollreveal'
+import { FaPeopleGroup } from "react-icons/fa6";
+import { MdOutlineTimer } from "react-icons/md";
+import { MdMoneyOff } from "react-icons/md";
 
 const Main = () => {
 
@@ -26,21 +29,31 @@ useEffect(() => {
 
                <div id='center-background' className='area_info_promotion'>
                   <h3>Promoções</h3>
+
                   <div className="all-cards">
                      <div className='cards-days'>
-                        <span className='name-days'>Domingo</span>
+                        <FaPeopleGroup size={41}/>
+                        <span className='name-days'>Família</span>
+                        <p>Domingo a noite é dia da família se reunir, conversar, rir e saborear uma maravilhosa pizza. Mas agora, você não precisa mais ligar para pedir, nós enviaremos todos os domingos para você.</p>
                      </div>
 
-                     <div className='cards-days'>
-                        <span className='name-days'>Terça</span>
-                     </div>
 
                      <div className='cards-days'>
-                        <span className='name-days'>Quinta</span>
+                        <MdOutlineTimer size={41}/>
+                        <span className='name-days'>Horário</span>
+                        <p>Sabe aquela sensação de ficar aguardando a pizza chegar e ela nunca chega? Pois é, aqui sua pizza chegará sempre no horário programado por você. Nosso intervalo de espera nunca supera 15 minutos.</p>
+                     </div>
+
+
+                     <div className='cards-days'>
+                        <MdMoneyOff size={41}/>
+                        <span className='name-days'>Preço</span>
+                        <p>Quando o entregador chegar na sua casa, você só recebe, nem precisa pagar. Por um preço justo, você pagará apenas pela frequência que você deseja receber sua pizza.</p>
                      </div>
                   </div>
+
                </div>
-               
+            
                <div id='background-right'>
                   
                </div>
