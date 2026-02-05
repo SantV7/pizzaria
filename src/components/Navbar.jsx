6b19ed964381cb2b11b7ac3c1d57b85a0666085a
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-02-04 21:44:43 -0300

    Fixing styling and resolving problemns

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,11 @@
-import React from 'react'
 import '../styles/navbar.css'
 
 const Navbar = () => {
 
-
-  
   return (
     <>
         <nav>
-            <ul>
+            <ul id='ul-desktop'>
                 <li><a href="#header">Inicio</a></li>
                 <li><a href="#promotion-section">Promoções</a></li>
                 <li><a href="#planSection">Pizzas</a></li>
