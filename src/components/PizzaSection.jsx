aa2f6dcaf40ae20d9cdb3cc9f3575ed37476ca4b
Vinícius Santos <3izuna@gmail.com>
2026-02-12 21:00:57 -0300

    Adding gsap effects

--- src/components/PizzaSection.jsx
+++ src/components/PizzaSection.jsx
@@ -1,6 +1,43 @@
 
+import gsap from 'gsap'
 import '../styles/pizzasection.css'
+import { useEffect } from 'react'
 const PizzaSection = () => {
+
+  useEffect(() => {
+    gsap.fromTo('#pizza-options-1', {
+      x: -200,
+      opacity: 0
+    }, {
+      x: 0,
+      duration: 1,
+      opacity: 1,
+      ease: 'power2',
+      delay: 0.1
+    })
+
+    gsap.fromTo('#pizza-options-2', {
+      x: 200,
+      opacity: 0
+    }, {
+      x: 0,
+      duration: 1,
+      delay: 0.25,
+      opacity: 1,
+      ease: 'power2'
+    })   
+    
+    gsap.fromTo('#pizza-options-3', {
+      y: 100,
+      opacity: 0
+    }, {
+      y: 0,
+      duration: 1,
+      delay: 0.25,
+      opacity: 1,
+      ease: 'power2'
+    })      
+  }, [])
   return (
     <>
         <section id='planSection'>
@@ -20,9 +57,9 @@ const PizzaSection = () => {
 
           <article id='sabores-area'>
               <div className='area-options'>
-                  <div className="pizza-options">
+                  <div  className="pizza-options">
                       <h3>Sabores Tradicionais</h3>
-                    <ul>
+                    <ul id='pizza-options-1'>
                         <li>Portuguesa</li>
                         <li>Mussarela</li>
                         <li>Calabresa</li>
@@ -33,7 +70,7 @@ const PizzaSection = () => {
 
                   <div className="pizza-options">
                       <h3>Bordas</h3>
-                    <ul>
+                    <ul id='pizza-options-2'>
                         <li>Cheedar</li>
                         <li>Catupiry</li>
                         <li>Chocolate amargo</li>
@@ -43,7 +80,7 @@ const PizzaSection = () => {
 
                   <div className="pizza-options">
                       <h3>Sabores Especiais</h3>
-                    <ul>
+                    <ul id='pizza-options-3'>
                         <li>Camarão</li>
                         <li>Romeu e julieta</li>
                         <li>Chocolate</li>
